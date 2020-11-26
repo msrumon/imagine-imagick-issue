0f6d3f179c168393d0a39c6ae29319ace13ceaa7
Rumon <crash.msr@gmail.com>
2020-11-26 22:57:00 +0600

    Produced: Cannot open uploaded EPS file directly

--- index.php
+++ index.php
@@ -5,12 +5,40 @@ use Imagine\Imagick\Imagine;
 
 require_once './vendor/autoload.php';
 
-try {
-    $imagine = new Imagine();
-    $image = $imagine->open('Untitled.eps');
-    echo 'Image:' . PHP_EOL;
-    var_dump($image);
-} catch (RuntimeException $ex) {
-    echo 'Runtime Exception:' . PHP_EOL;
-    var_dump($ex);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $tmpPath = $_FILES['image']['tmp_name'];
+    if (is_uploaded_file($tmpPath)) {
+        try {
+            $imagine = new Imagine();
+            $image = $imagine->open($tmpPath);
+            echo 'Image uploaded! Metadata:' . PHP_EOL;
+            var_dump($image->metadata());
+            echo PHP_EOL;
+        } catch (RuntimeException $ex) {
+            echo 'Runtime Exception:' . PHP_EOL;
+            var_dump($ex);
+            echo PHP_EOL;
+        }
+    }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        <label for="img">Choose an image:</label>
+        <input type="file" name="image" id="img">
+        <button>Upload</button>
+    </form>
+</body>
+
+</html>
